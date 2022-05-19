92d8133e3ec0081afa3297e441d75d0577c1276d
Dakkshesh <dakkshesh5@gmail.com>
2022-05-20 06:01:02 +0700

    build-tc: script import from https://github.com/dakkshesh07/tc-build-1/blob/master/build-tc.sh into Head
    Change-Id: I7fd2c40f642ddf64740999ea7f7e0c308cb57fce
    Signed-off-by: ben863 <r.budhiono@gmail.com>

--- build-tc.sh
+++ build-tc.sh
@@ -7,7 +7,9 @@
 # GH_USERNAME | Your Github Username
 # GH_EMAIL | Your Github Email
 # GH_TOKEN | Your Github Token ( repo & repo_hook )
-# GH_PUSH_REPO_URL | Your Repository for store compiled Toolchain ( without https:// or www. ) ex. github.com/xyz-prjkt/xRageTC.git
+# GL_TOKEN | Your GitLab Token (read_user, read_repository, write_repository)
+# GH_PUSH_REPO_URL | Your GitHub Repository for store compiled Toolchain ( without https:// or www. ) ex. github.com/cbendot/elastics-clang.git
+# GL_PUSH_REPO_URL | Your GitLab Repository
 
 # Function to show an informational message
 msg() {
@@ -28,7 +30,6 @@ tg_post_msg() {
 	-d "disable_web_page_preview=true" \
 	-d "parse_mode=html" \
 	-d text="$1"
-
 }
 tg_post_build() {
 	curl --progress-bar -F document=@"$1" "$BOT_MSG_URL" \
@@ -42,13 +43,14 @@ tg_post_build() {
 rel_date="$(date "+%Y%m%d")" # ISO 8601 format
 rel_friendly_date="$(date "+%B %-d, %Y")" # "Month day, year" format
 builder_commit="$(git rev-parse HEAD)"
+START=$(date +"%s")
 
 # Send a notificaton to TG
-tg_post_msg "<b>$LLVM_NAME: Toolchain Compilation Started</b>%0A<b>Date : </b><code>$rel_friendly_date</code>%0A<b>Toolchain Script Commit : </b><code>$builder_commit</code>%0A"
+tg_post_msg "<b>Elastics Clang Compilation Started</b>%0A<b>Vendor : </b><code>$LLVM_NAME clang</code>%0A<b>Toolchain Script Commit : </b><code>$builder_commit</code>%0A<b>Build Date : </b><code>$rel_friendly_date</code>"
 
 # Build LLVM
-msg "$LLVM_NAME: Building LLVM..."
-tg_post_msg "<b>$LLVM_NAME: Building LLVM. . .</b>"
+msg "Building LLVM..."
+tg_post_msg "<b>Building LLVM...</b>"
 ./build-llvm.py \
 	--clang-vendor "$LLVM_NAME" \
 	--projects "clang;lld;polly" \
@@ -65,8 +67,8 @@ tg_post_msg "<b>$LLVM_NAME: Building LLVM. . .</b>"
 }
 
 # Build binutils
-msg "$LLVM_NAME: Building binutils..."
-tg_post_msg "<b>$LLVM_NAME: Building Binutils. . .</b>"
+msg "Building Binutils..."
+tg_post_msg "<b>Building Binutils...</b>"
 ./build-binutils.py --targets arm aarch64
 
 # Remove unused products
@@ -97,24 +99,36 @@ llvm_commit_url="https://github.com/llvm/llvm-project/commit/$short_llvm_commit"
 binutils_ver="$(ls | grep "^binutils-" | sed "s/binutils-//g")"
 clang_version="$(install/bin/clang --version | head -n1 | cut -d' ' -f4)"
 
-tg_post_msg "<b>$LLVM_NAME: Toolchain compilation Finished</b>%0A<b>Clang Version : </b><code>$clang_version</code>%0A<b>LLVM Commit : </b><code>$llvm_commit_url</code>%0A<b>Binutils Version : </b><code>$binutils_ver</code>"
+tg_post_msg "<b>Pushed to Repository Started...</b>%0A<b>Clang Version : </b><code>$clang_version</code>%0A<b>Binutils Version : </b><code>$binutils_ver</code>%0A<b>LLVM Commit: </b><code>$llvm_commit_url</code>%0A<b>Builder Commit: </b><code>https://github.com/cbendot/tcbuild/commit/$builder_commit</code>%0A<b>Elastics Clang Bump to: </b><code>$rel_date build</code>"
 
 # Push to GitHub
 # Update Git repository
 git config --global user.name $GH_USERNAME
 git config --global user.email $GH_EMAIL
-git clone "https://$GH_USERNAME:$GH_TOKEN@$GH_PUSH_REPO_URL" rel_repo
+git clone "https://cbendot:$GH_TOKEN@$GH_PUSH_REPO_URL" rel_repo
 pushd rel_repo || exit
 rm -fr ./*
 cp -r ../install/* .
 git checkout README.md # keep this as it's not part of the toolchain itself
 git add .
-git commit -asm "$LLVM_NAME: Bump to $rel_date build
+git commit -asm "$LLVM_NAME Clang: Bump to $rel_date build
 
 LLVM commit: $llvm_commit_url
 Clang Version: $clang_version
 Binutils version: $binutils_ver
-Builder commit: https://$GH_PUSH_REPO_URL/commit/$builder_commit"
+Builder commit: https://github.com/cbendot/tcbuild/commit/$builder_commit"
+
+# Downgrade the HTTP version to 1.1
+git config --global http.version HTTP/1.1
+# Increase git buffer size
+git config --global http.postBuffer 55428800
+
 git push -f
 popd || exit
-tg_post_msg "<b>$LLVM_NAME: Toolchain pushed to <code>https://$GH_PUSH_REPO_URL</code></b>"
+
+# Set git buffer to original size
+git config --global http.version HTTP/2
+
+END=$(date +"%s")
+DIFF=$(($END - $START))
+tg_post_msg "<b>Pushed to Repository Done !</b>%0A%0A<code>$((DIFF / 60)) minute(s) $((DIFF % 60)) second(s) </code>"
